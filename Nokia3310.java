6e8a3734028f9dd00865779ad5834c87e37fdd64
oladapo444 <151373552+oladapo444@users.noreply.github.com>
2023-12-04 15:38:22 +0100

    Add files via upload

---
+++ Nokia3310.java
@@ -0,0 +1,194 @@
+import java.util.Scanner;
+
+public class Nokia3310 {
+public static void main(String [] args) {
+Scanner scanner = new scanner(System.in);
+System.out.print("Enter 1 to 13 to  access phone menu ");
+System.out.println("""
+					1. Phone book
+					2. Message
+					3. Chat
+					4. Call Register
+					5. Tones
+					6. Settings
+					7 Divert
+					8. games
+					9. Calculation
+					10.Reminder
+					11. Clock
+					12 profile
+					13. Sim service   """);
+int menu = scanner.nextInt();
+switch(menu) {
+case 1:
+System.out.println("Enter 1 to 10 to access phone book");
+System.out.print("1. search\n2. service Nos
+\n3.  Add name\n4.  Erase\n5. Edit \n6.  Assign tone\n7. Send b'card\n8. option\n9.  Speed dials\n10.  voice tags
+int  Phone book = NextInt
+Switch(Phone book){
+case 1: System.out.println("search");
+break;
+       
+case 2:System.out.println("Service Nos");
+break; 
+                  
+     case 3 :System.out.println("add Name");
+break;
+
+  case 4: System.out.println("Erase");
+break;
+
+case 5:System.out.println("Edit");
+break;
+
+  case 6:  System.out.println("Assign tone");
+break;            
+case 7:System.out.println("Send b'card");
+break;         
+case 8: System.out.println("option");
+break;
+case 9:Systesm.out.println("Speed dials"); 
+break;
+case 10:System.out.println("Voice tags");
+    break;
+}
+
+Case 2:
+System,out.println("Enter  1 to 10 to access Messages");
+System.out.print("1. write message\n2. inbox\n3.  outbox\n4.  picture messages\n5.  Templates \n6.  Smiley\n7.  Message Settings \n8. info sevice\n9 . info mailbox number\n10.  service command editor
+int  Messages = NextInt();
+Switch(Messages){
+case 1:System.out.println("write message");
+break;
+case2:System.out.println("inbox");
+break;
+case3:System.out.println("outbox");
+break;
+case4:System.out.println("picture message");
+break;
+case5:System.out.println("Templates");
+break;
+case6:System.out.println("Smiley");
+break;
+case7:System.out.println("Message Settings");
+break;
+case8:System.out.println("info service");
+break;
+case9:System.out.println("info mailbox number");
+break;
+case10:System.out.println("Service command editor");
+break;
+}
+Case 3:
+ System.out.println("chat");
+break;
+
+Case 4:
+System.outprintln("Enter 1 to 8 to access Call register")
+System.out.print("1. Missed calls\n2. Received calls\n3.  dialled numbers\n4.  Ease recent call lists\n5.  Show call duration \n6.  Show call costs\n7.  call cost settings \n8. prepaid credit
+int  Messages = NextInt
+Switch(Call register) {
+case 1:System.out.println("Missed calls");
+break;
+case2:System.out.println("Received calls");
+break;
+case3:System.out.println("Dialled numbers");
+break;
+case4:System.out.println("Erase recent call lists");
+break;
+case5:System.out.println("show call duration");
+break;
+case6:System.out.println("show call costs");
+break;
+case7:System.out.println("call cost settings");
+break;
+case 8:System.out.println("prepaid credit");
+break;
+}
+
+Case 5:
+ System.out.print("Enter 1 to 9 to access Tones")
+System.out.println("1Ringing tone\n2Ringing volume\n3 Incoming call alert\n4 Composer\n5 message alert tone\n6Keypad tones \n7Warning and games tones  \n8verberting alert\n9Screen saver 
+int  Messages = NextInt
+Switch(Tone){
+case 1:System.out.println("Ringing tone");
+break;
+case2:System.out.println("Ringing volume");
+break;
+case3:System.out.println("incoming call alert");
+break;
+case4:System.out.println("Composer");
+break;
+case5:System.out.println("Message alert tone");
+break;
+case6:System.out.println("Keypad tones");
+break;
+case7:System.out.println("Warning and games tones");
+break;
+case 8:System.out.println("verberting alert");
+break;
+Case 9:System.out.println("Screen saver");
+break;
+}
+
+
+
+Case 6:
+ System.out.print("Enter to1 to 4 to access Settings");
+System.out.print("1call settings\n2Phone settings\n3 Security settings\n4 Restore factory settings
+int  Settings = NextInt
+Switch(Settings){
+case 1:System.out.println("call settings");
+break;
+case2:System.out.println("Phone Settings");
+break;
+case3:System.out.println("Security settings");
+break;
+case4:System.out.println("Restore factory settings");
+break;
+}
+
+case 7: 
+
+System.out.println("Call divert")
+
+
+Case 8:
+
+ System.out.println("Games");
+
+Case 9:
+
+ System.out.println("Calculator");
+
+Case 10:
+ System.out.println("Reminders");
+
+Case 11:
+ System.out.println("Clock");
+System.out.print("Enter 1 to 6 to access Clock")
+System.out.println("1. Alarm clock\n2.Clock settings\n3. Date settings\n4 Stopwatch\n5 Countdown timer\n6 Auto update of date and time\n");
+int  Clock = NextInt
+Switch(Clock){
+case 1:System.out.println("Alarm clock");
+break;
+case 2:System.out.println("Clock settings");
+break;
+case 3:System.out.println("Date settings");
+break;
+case 4:System.out.println("Stopwatch");
+break;
+case 5:System.out.println(" Countdown timer");
+break;
+case 6:System.out.println("Auto update of date and time");
+break;
+}
+
+Case 12:
+ System.out.println("profiles");
+case 13:
+ System.out.println("Sim services")
+
+            }
+      }
+}
